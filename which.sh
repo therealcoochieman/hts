637b0024b36935d559ec3e3228b28b4bd6f790ed
Théo De Magalhaes <theo.de-magalhaes@epita.fr>
2024-03-30 14:31:46 +0100

    pls

--- which.sh
+++ which.sh
@@ -1,46 +1,80 @@
-# !/bin/ksh
+#!/bin/ksh
 
+USAGE="usage: which [-a] name ..."
+SHOW_ALL=false
 while getopts ":a" opt; do
-  case $opt in
-    a)
-      show_all=true
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-  esac
+	case ${opt} in
+		a)
+			SHOW_ALL=true
+			;;
+		?)
+	        >&2 echo "which: unknown option -- ${OPTARG}"
+	        >&2 echo $usage
+			;;
+	esac
 done
 
-shift $((OPTIND - 1))
+ARGS=$#
+TOTAL_NOT_FOUND=0
+EXIT_CODE=0
+FOUND=false
+DEF_PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11R6/bin:/usr/local/bin:/usr/local/sbin"
+
+function find_in_path {
+		if [ -f $1 ] && [ -x $1 ]; then
+			echo $1
+            return 1
+		fi
+        return 0
+}
+
+shift $(( $OPTIND - 1 ))
 
 if [ $# -eq 0 ]; then
-  echo "Usage: $0 [-a] name ..."
-  exit 1
+	>&2 echo $USAGE
 fi
 
-search_path=$(echo "$PATH" | tr ":" "\n")
 
-found=false
-
-for name in "$@"; do
-  while IFS= read -r dir; do
-    file_path="$dir/$name"
-    if [ -x "$file_path" ]; then
-        echo "$file_path"
-        found=true
-      if [ "$show_all" = false ]; then
-        break
-      fi
-    fi
-  done <<< "$search_path"
-if [ "$found" = false ]; then
-    >&2 echo "$name: Command not found."
+if [ ${#PATH} -eq 0 ]; then
+	PATH=$DEF_PATH
+	# echo $PATH
 fi
+
+while [ $# -ne 0 ]; do
+	IFS=':'
+	MATCH=$(echo $1 | cut -c 1-1)
+	if [ $MATCH = "/" ]; then
+        find_in_path $1 
+        if [ $? -eq 1 ]; then
+			FOUND=true
+        fi
+	else
+		for path in $PATH; do
+			if [ ${#path} -eq 0 ]; then
+				path=.
+				echo $path
+			fi
+			tmp=$path/$1
+            find_in_path $tmp
+			if [ $? -eq 1 ]; then
+				FOUND=true
+				if [ $SHOW_ALL = false ]; then
+					break
+				fi
+			fi
+		done
+	fi
+	if [ $FOUND = false ]; then
+	    >&2 echo "which: $1: Command not found."
+		TOTAL_NOT_FOUND=$(( $TOTAL_NOT_FOUND + 1 ))
+		RETURN_CODE=1
+	fi
+	
+	FOUND=false
+	shift
 done
-
-if [ "$found" = false ]; then
-  exit 2
+if [ $ARGS -eq $TOTAL_NOT_FOUND ]; then
+	exit 2
 fi
+exit $RETURN_CODE
 
-exit 0
